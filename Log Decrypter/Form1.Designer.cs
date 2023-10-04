d8d58facb5bde7f2ceb3c206001b4fb3df89f9dc
Shashiraj Walsetwar <shashiraj.walsetwar@sikka.ai>
2023-10-04 11:17:17 -0700

    Version 1: log decryption and display in plain text

---
+++ Log Decrypter/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Log_Decrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.logDisplay = new System.Windows.Forms.RichTextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.filePath_textbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(592, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // logDisplay
+            // 
+            this.logDisplay.Location = new System.Drawing.Point(27, 108);
+            this.logDisplay.Name = "logDisplay";
+            this.logDisplay.Size = new System.Drawing.Size(701, 340);
+            this.logDisplay.TabIndex = 2;
+            this.logDisplay.Text = "";
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(348, 454);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // filePath_textbox
+            // 
+            this.filePath_textbox.Location = new System.Drawing.Point(27, 51);
+            this.filePath_textbox.Name = "filePath_textbox";
+            this.filePath_textbox.Size = new System.Drawing.Size(521, 22);
+            this.filePath_textbox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 498);
+            this.Controls.Add(this.filePath_textbox);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.logDisplay);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Log Decryptor Utility";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox logDisplay;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.TextBox filePath_textbox;
+    }
+}
+
---
+++ Log Decrypter/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace Log_Decrypter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            
+            AesEncryptor encryptor = new AesEncryptor();
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    using (StreamReader read = new StreamReader(filePath))
+                    {
+                        string line;
+                        while ((line = read.ReadLine()) != null)
+                        {
+                            //Console.WriteLine(line);
+                            string decrypted_line = encryptor.DecryptString(line);
+                            logDisplay.AppendText(decrypted_line+ "\n");
+                        }
+                    }
+                    
+                }
+            }
+            filePath_textbox.Text = filePath;
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            string location = System.AppDomain.CurrentDomain.BaseDirectory;
+            //System.IO.StreamWriter file = new System.IO.StreamWriter($"{location}\\decryptedLogs.txt");
+
+            System.IO.File.WriteAllText($"{location}\\decryptedLog.txt", logDisplay.Text.Replace("\n", Environment.NewLine));
+            MessageBox.Show($"Decrypted Logs stored at: {location}");
+        }
+    }
+
+    public class AesEncryptor : IDisposable
+    {
+        private RijndaelManaged _rijndael;
+        private byte[] _key;
+
+        public AesEncryptor()
+        {
+            string encryptionKey = "?;kx7$^Pb;g2ftuIj,4]o8;~S>[G(}/|";
+            _rijndael = new RijndaelManaged();
+            _key = Encoding.UTF8.GetBytes(encryptionKey);  // Make sure this key length matches Rijndael's requirements
+            _rijndael.Key = _key;
+        }
+
+        public string EncryptString(string plainText)
+        {
+            _rijndael.GenerateIV();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                ms.Write(_rijndael.IV, 0, _rijndael.IV.Length);
+
+                using (CryptoStream cs = new CryptoStream(ms, _rijndael.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+                    cs.Write(plainBytes, 0, plainBytes.Length);
+                }
+
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        public string DecryptString(string encryptedText)
+        {
+            byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
+
+            using (MemoryStream ms = new MemoryStream(encryptedBytes))
+            {
+                byte[] iv = new byte[16];
+                ms.Read(iv, 0, iv.Length);
+                _rijndael.IV = iv;
+
+                using (CryptoStream cs = new CryptoStream(ms, _rijndael.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    using (StreamReader reader = new StreamReader(cs))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_rijndael != null)
+            {
+                _rijndael.Clear();
+                _rijndael = null;
+            }
+        }
+    }
+}
